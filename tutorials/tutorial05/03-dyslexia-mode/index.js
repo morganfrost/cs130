33e0632eacab717f6b4d24d7f66cbadaa03cd75f
morganfrost <morganfrost2024@u.northwestern.edu>
2022-04-29 20:36:49 -0500

    tutorial05 commit

--- tutorials/tutorial05/03-dyslexia-mode/index.js
+++ tutorials/tutorial05/03-dyslexia-mode/index.js
@@ -2,3 +2,16 @@
   See Smashing Magazine Tutorial:
   https://www.smashingmagazine.com/2021/11/dyslexia-friendly-mode-website/
 */
+
+const themeDyslexiaMode = () => {
+  console.log('Switch to Dyslexia Mode');
+  document.querySelector("body").className = "dyslexia-mode";
+}
+
+const themeOriginalMode = () => {
+  console.log('Switch back to Original Mode');
+  document.querySelector("body").className = "default";
+}
+
+document.querySelector("#dyslexia-toggle").addEventListener('click', themeDyslexiaMode);
+document.querySelector("#original-toggle").addEventListener('click', themeOriginalMode);
